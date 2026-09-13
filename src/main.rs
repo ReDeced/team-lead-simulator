779aa37d04736cf69f2ff872b9bd34ab1d60db61
ReDeced <romanzorin322@gmail.com>
2026-09-13 18:15:37 +0300

    Выводит на экран список разработчиков

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use macroquad::prelude::*;
+
+mod types;
+
+use crate::types::load_developers;
+
+
+#[macroquad::main("cocal")]
+async fn main() {
+    const FONT_BYTES: &[u8] = include_bytes!("../assets/verdana-font.ttf");
+    let font = load_ttf_font_from_bytes(FONT_BYTES).expect("Не удалось загрузить шрифт");
+
+    let developers = load_developers();
+
+    loop {
+        clear_background(DARKPURPLE);
+        for (i, developer) in developers.iter().enumerate() {
+            draw_text_ex(format!("{} {} лет {:?} {:?}", developer.name, developer.age, developer.position, developer.grade),
+            20.0, 40.0 + 30.0 * (i as f32),
+            TextParams { font: Some(&font), font_size: 20, font_scale: 1.0, font_scale_aspect: 1.0, rotation: 0.0, color: WHITE });
+        }
+        next_frame().await;
+    }
+}
+
